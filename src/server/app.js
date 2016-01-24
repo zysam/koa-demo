cb238d85dca569555e602b0210f2274113fae908
zysam <explnwind@gmail.com>
2016-01-24 20:07:30 +0800

    init: sign!

---
+++ src/server/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const app = require('koa')();
+const co = require('co');
+const config = require('./config');
+const dbInit = require('./config/db');
+const koaInit = require('./config/koa');
+
+app.init = co.wrap(function*() {
+  yield dbInit();
+  koaInit(app);
+  app.server = app.listen(config.app.port); // 扩展 server 的方法
+});
+
+if (!module.parent) {
+  app
+  .init()
+  .then(function() {
+    console.log('%s is listening port: %d !!!', config.app.name, config.app.port);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+}
+
+module.exports = app;
---
+++ src/server/config/db.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const config = require('../config');
+
+module.exports = function() {
+  return new Promise(function(resolve, reject) {
+    mongoose.connect(config.mongo.url, function(err) {
+      if (err) {
+        console.error('connect to %s error', config.mongo.url, err.message);
+        process.exit(1);
+      }
+      console.info(`mongodb connected: ${config.mongo.url} !!!`);
+      return resolve();
+    });
+  });
+};
